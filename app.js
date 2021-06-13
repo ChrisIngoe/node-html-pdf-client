8c5be9464693ab09e6d6d5e98ba9fbdaea1fe3e5
Chris Ingoe <chris.software.engineer@gmail.com>
2021-06-13 19:52:18 +0100

    code base

---
+++ app.js
@@ -0,0 +1,40 @@
+const axios = require('axios'),
+  fs = require('fs');
+
+const fileNameTest1 = 'test1.html';
+const fileNameTest2 = 'test2.html';
+const fileNameTest3 = 'test3.html';
+
+const fileTest1 = fs.readFileSync(`./${fileNameTest1}`, { encoding: 'base64' });
+const fileTest2 = fs.readFileSync(`./${fileNameTest2}`, { encoding: 'base64' });
+const fileTest3 = fs.readFileSync(`./${fileNameTest3}`, { encoding: 'base64' });
+const fileArray = [
+  { fileName: fileNameTest1, data: fileTest1 },
+  //  { fileName: fileNameTest2, data: fileTest2 },
+  //  { fileName: fileNameTest3, data: fileTest3 },
+];
+
+axios
+  .post('http://localhost:3005/files/convert/pdftohtml/base64', fileArray, {
+    headers: {
+      ['Content-Type']: 'application/json',
+      ['Accept']: 'application/json',
+    },
+  })
+  .then(function (response) {
+    console.log(`${response.data.length} files processed`);
+    for (let i = 0; i < response.data.length; i++) {
+      const file = response.data[i];
+      fs.writeFileSync(
+        `./${file.fileName}`,
+        Buffer.from(response.data[i].data, 'base64')
+      );
+      console.log(`${file.fileName} saved locally to pdf file format`);
+    }
+  })
+  .catch(function (error) {
+    console.error(error.response?.statusText || error);
+  })
+  .then(function () {
+    console.log('Finished');
+  });
